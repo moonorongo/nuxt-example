81e1527b44c67ca18fff49e0cece56c986fe4b32
Mauro <mauro.cifuentes@ingamana.com>
2022-12-29 10:59:41 -0300

    fix server side script issue

--- store/data/actions.js
+++ store/data/actions.js
@@ -1,7 +1,9 @@
 const fs = process.server ? require("fs-extra") : null;
 const path = process.server ? require("path") : null;
 
-const MEMBERS_CACHE_PATH = path.join(process.cwd(), "dist", "globalData.json");
+const MEMBERS_CACHE_PATH = process.server
+  ? path.join(process.cwd(), "dist", "globalData.json")
+  : null;
 
 export default {
   modifyData(context, data) {
@@ -10,31 +12,43 @@ export default {
 
   async fetchData(context) {
     let cachedData;
-    try {
-      cachedData = JSON.parse(fs.readFileSync(MEMBERS_CACHE_PATH, "utf8"));
-    } catch (error) {
-      console.log("💾 CACHE NOT INITIALIZED");
-    }
 
-    if (!cachedData) {
+    if (process.env.NODE_ENV === "production") {
+      console.log("PRODUCTION API");
+      try {
+        cachedData = JSON.parse(fs.readFileSync(MEMBERS_CACHE_PATH, "utf8"));
+      } catch (error) {
+        console.log("💾 CACHE NOT INITIALIZED");
+      }
+
+      if (!cachedData) {
+        const response = await fetch(
+          "https://rickandmortyapi.com/api/character?page=0"
+        );
+        const data = await response.json();
+
+        try {
+          await fs.writeFileSync(
+            MEMBERS_CACHE_PATH,
+            JSON.stringify(data),
+            (err) => {
+              if (err) throw err;
+            }
+          );
+          console.log("💾 CACHE FILE WRITTEN SUCCESSFULLY");
+        } catch (error) {
+          console.log("💾 ERROR WRITING MEMBERS CACHE TO FILE\n", error);
+        }
+
+        cachedData = data;
+      }
+    } else {
+      console.log("DEVELOPMENT API");
+
       const response = await fetch(
         "https://rickandmortyapi.com/api/character?page=0"
       );
       const data = await response.json();
-
-      try {
-        await fs.writeFileSync(
-          MEMBERS_CACHE_PATH,
-          JSON.stringify(data),
-          (err) => {
-            if (err) throw err;
-          }
-        );
-        console.log("💾 CACHE FILE WRITTEN SUCCESSFULLY");
-      } catch (error) {
-        console.log("💾 ERROR WRITING MEMBERS CACHE TO FILE\n", error);
-      }
-
       cachedData = data;
     }
 
